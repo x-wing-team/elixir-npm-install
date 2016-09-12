ece231e79598a307d21102de86a5ff595c620c2d
Adam Synnott <info@adamsynnott.id.au>
2016-09-13 00:45:11 +1000

    added Jeremy's code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var elixir = require('laravel-elixir');
 var gulp = require('gulp');
 var install = require('gulp-install');
 
-elixir.extend('npm-install', function(src, options) {
+elixir.extend('install', function(src, options) {
 
     var Task = elixir.Task;
 
-    new Task('npm-install',function() {
+    new Task('install',function() {
 
         return gulp.src(['./package.json'])
             .pipe(install());
